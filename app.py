61e792b3914a9ffaa4ef6aef56381c3a5e9aa648
renoviana <renotviana@gmail.com>
2019-11-03 20:50:03 -0300

    Api Final

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/<serie>')
-def getSerie(serie):
+def getData(serie):
+    soup = BeautifulSoup(requests.get(
+        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
+    data = json.loads(re.search(r"classificacao = (.*?);",
+                                soup.find("script", {"id": "scriptReact"}).text).group(1))
+    return data
+
+
+def getDataAndArtilheiros(serie):
     soup = BeautifulSoup(requests.get(
         "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
     data = json.loads(re.search(r"classificacao = (.*?);",
@@ -20,149 +27,65 @@ def getSerie(serie):
                             "nome": jogador.find("div", {"class": "jogador-nome"}).text,
                             "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
                             } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify(brasileirao.to_dict())
+    return data
+
+
+@app.route('/<serie>')
+def getSerie(serie):
+    return jsonify(Brasileirao.from_dict(getDataAndArtilheiros(serie)).to_dict())
 
 
 @app.route('/<serie>/classificacao')
 def getClassificacao(serie):
-    soup = BeautifulSoup(requests.get(
-        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
-    data = json.loads(re.search(r"classificacao = (.*?);",
-                                soup.find("script", {"id": "scriptReact"}).text).group(1))
-    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
-                                 ).find_all("div", {"class": "jogador"})
-    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
-                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
-                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
-                            } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify([classificacao.to_dict() for classificacao in brasileirao.classificacao])
+    return jsonify([classificacao.to_dict() for classificacao in Brasileirao.from_dict(getData(serie).classificacao])
 
 
 @app.route('/<serie>/edicao')
 def getEdicao(serie):
-    soup = BeautifulSoup(requests.get(
-        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
-    data = json.loads(re.search(r"classificacao = (.*?);",
-                                soup.find("script", {"id": "scriptReact"}).text).group(1))
-    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
-                                 ).find_all("div", {"class": "jogador"})
-    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
-                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
-                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
-                            } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify(brasileirao.edicao.to_dict())
+    return jsonify(Brasileirao.from_dict(getData(serie)).to_dict)
 
 
 @app.route('/<serie>/lista_jogos')
 def getListaJogos(serie):
-    soup = BeautifulSoup(requests.get(
-        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
-    data = json.loads(re.search(r"classificacao = (.*?);",
-                                soup.find("script", {"id": "scriptReact"}).text).group(1))
-    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
-                                 ).find_all("div", {"class": "jogador"})
-    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
-                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
-                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
-                            } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify([lista_jogos.to_dict() for lista_jogos in brasileirao.lista_jogos])
+    return jsonify([lista_jogos.to_dict() for lista_jogos in Brasileirao.from_dict(getData(serie)).lista_jogos])
 
 
 @app.route('/<serie>/artilheiros')
 def getArtilheiros(serie):
-    soup = BeautifulSoup(requests.get(
-        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
-    data = json.loads(re.search(r"classificacao = (.*?);",
-                                soup.find("script", {"id": "scriptReact"}).text).group(1))
-    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
-                                 ).find_all("div", {"class": "jogador"})
-    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
-                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
-                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
-                            } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify([lista_jogos.to_dict() for lista_jogos in brasileirao.artilheiros])
+    return jsonify(getDataAndArtilheiros(serie).artilheiros.to_dict)
 
 
 @app.route('/<serie>/faixas_classificacao')
 def getFaixasClassificacao(serie):
-    soup = BeautifulSoup(requests.get(
-        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
-    data = json.loads(re.search(r"classificacao = (.*?);",
-                                soup.find("script", {"id": "scriptReact"}).text).group(1))
-    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
-                                 ).find_all("div", {"class": "jogador"})
-    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
-                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
-                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
-                            } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify([faixa.to_dict() for faixa in brasileirao.faixas_classificacao])
+    return jsonify([faixa.to_dict() for faixa in Brasileirao.from_dict(getData(serie)).faixas_classificacao])
 
 
 @app.route('/<serie>/fase')
 def getFase(serie):
-    soup = BeautifulSoup(requests.get(
-        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
-    data = json.loads(re.search(r"classificacao = (.*?);",
-                                soup.find("script", {"id": "scriptReact"}).text).group(1))
-    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
-                                 ).find_all("div", {"class": "jogador"})
-    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
-                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
-                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
-                            } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify(brasileirao.fase.to_dict())
+    return jsonify(Brasileirao.from_dict(getData).fase.to_dict)
 
 
 @app.route('/<serie>/fases_navegacao')
 def getFasesNavegacao(serie):
-    soup = BeautifulSoup(requests.get(
-        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
-    data = json.loads(re.search(r"classificacao = (.*?);",
-                                soup.find("script", {"id": "scriptReact"}).text).group(1))
-    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
-                                 ).find_all("div", {"class": "jogador"})
-    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
-                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
-                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
-                            } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify([fases.to_dict() for fases in brasileirao.fases_navegacao])
+    return jsonify([fases.to_dict() for fases in Brasileirao.from_dict(getData(serie)).fases_navegacao])
 
 
 @app.route('/<serie>/rodada')
 def getRodadaAtual(serie):
-    soup = BeautifulSoup(requests.get(
-        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
-    data = json.loads(re.search(r"classificacao = (.*?);",
-                                soup.find("script", {"id": "scriptReact"}).text).group(1))
-    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
-                                 ).find_all("div", {"class": "jogador"})
-    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
-                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
-                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
-                            } for jogador in data_artilheiros]
-    brasileirao = Brasileirao.from_dict(data)
-    return jsonify(brasileirao.rodada.to_dict())
+    return jsonify(Brasileirao.from_dict(getData(serie)).rodada.to_dict())
 
 
 @app.route('/<serie>/rodada/<rodada>')
 def getRodada(serie, rodada):
-    url = url = "https://api.globoesporte.globo.com/tabela/d1a37fa4-e948-43a6-ba53-ab24ab3a45b1/fase/fase-unica-seriea-2019/rodada/{}/jogos/".format(
+    url=url="https://api.globoesporte.globo.com/tabela/d1a37fa4-e948-43a6-ba53-ab24ab3a45b1/fase/fase-unica-seriea-2019/rodada/{}/jogos/".format(
         rodada)
     if(serie == "b"):
-        url = "https://api.globoesporte.globo.com/tabela/009b5a68-dd09-46b8-95b3-293a2d494366/fase/fase-unica-serieb-2019/rodada/{}/jogos/".format(
+        url="https://api.globoesporte.globo.com/tabela/009b5a68-dd09-46b8-95b3-293a2d494366/fase/fase-unica-serieb-2019/rodada/{}/jogos/".format(
             rodada)
 
     return jsonify(requests.get(url).json())
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port=int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
