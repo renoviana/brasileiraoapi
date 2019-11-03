f2b406a820becf7c814917b9bf13bcdb45105a63
renoviana <renotviana@gmail.com>
2019-11-03 20:23:42 -0300

    first

--- app.py
+++ app.py
@@ -88,6 +88,70 @@ def getArtilheiros(serie):
     return jsonify([lista_jogos.to_dict() for lista_jogos in brasileirao.artilheiros])
 
 
+@app.route('/<serie>/faixas_classificacao')
+def getFaixasClassificacao(serie):
+    soup = BeautifulSoup(requests.get(
+        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
+    data = json.loads(re.search(r"classificacao = (.*?);",
+                                soup.find("script", {"id": "scriptReact"}).text).group(1))
+    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
+                                 ).find_all("div", {"class": "jogador"})
+    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
+                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
+                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
+                            } for jogador in data_artilheiros]
+    brasileirao = Brasileirao.from_dict(data)
+    return jsonify([faixa.to_dict() for faixa in brasileirao.faixas_classificacao])
+
+
+@app.route('/<serie>/fase')
+def getFase(serie):
+    soup = BeautifulSoup(requests.get(
+        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
+    data = json.loads(re.search(r"classificacao = (.*?);",
+                                soup.find("script", {"id": "scriptReact"}).text).group(1))
+    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
+                                 ).find_all("div", {"class": "jogador"})
+    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
+                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
+                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
+                            } for jogador in data_artilheiros]
+    brasileirao = Brasileirao.from_dict(data)
+    return jsonify(brasileirao.fase.to_dict)
+
+
+@app.route('/<serie>/fases_navegacao')
+def getFasesNavegacao(serie):
+    soup = BeautifulSoup(requests.get(
+        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
+    data = json.loads(re.search(r"classificacao = (.*?);",
+                                soup.find("script", {"id": "scriptReact"}).text).group(1))
+    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
+                                 ).find_all("div", {"class": "jogador"})
+    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
+                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
+                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
+                            } for jogador in data_artilheiros]
+    brasileirao = Brasileirao.from_dict(data)
+    return jsonify([fases.to_dict() for fases in brasileirao.fases_navegacao])
+
+
+@app.route('/<serie>/rodada')
+def getRodada(serie):
+    soup = BeautifulSoup(requests.get(
+        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
+    data = json.loads(re.search(r"classificacao = (.*?);",
+                                soup.find("script", {"id": "scriptReact"}).text).group(1))
+    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
+                                 ).find_all("div", {"class": "jogador"})
+    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
+                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
+                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
+                            } for jogador in data_artilheiros]
+    brasileirao = Brasileirao.from_dict(data)
+    return jsonify(brasileirao.rodada.to_dict)
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
