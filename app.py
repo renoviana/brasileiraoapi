afca7eb94633d46b828eda4e09e169a0c0592841
renoviana <renotviana@gmail.com>
2019-11-03 20:10:17 -0300

    first

--- app.py
+++ app.py
@@ -72,6 +72,22 @@ def getListaJogos(serie):
     return jsonify([lista_jogos.to_dict() for lista_jogos in brasileirao.lista_jogos])
 
 
+@app.route('/<serie>/artilheiros')
+def getListaJogos(serie):
+    soup = BeautifulSoup(requests.get(
+        "https://globoesporte.globo.com/futebol/brasileirao-serie-{}/".format(serie)).text, 'html.parser')
+    data = json.loads(re.search(r"classificacao = (.*?);",
+                                soup.find("script", {"id": "scriptReact"}).text).group(1))
+    data_artilheiros = soup.find("section", {"class": "artilharia-wrapper"}
+                                 ).find_all("div", {"class": "jogador"})
+    data["artilheiros"] = [{"time": jogador.find("div", {"class": "jogador-escudo"}).find("img")['alt'],
+                            "nome": jogador.find("div", {"class": "jogador-nome"}).text,
+                            "gols": int(jogador.find("div", {"class": "jogador-gols"}).text)
+                            } for jogador in data_artilheiros]
+    brasileirao = Brasileirao.from_dict(data)
+    return jsonify([lista_jogos.to_dict() for lista_jogos in brasileirao.artilheiros])
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
